3f879a18978e96b46f4f05b0190c4f1bbff1a675
SvenJuergens <SvenJuergens@users.noreply.github.com>
2021-11-07 22:53:35 +0100

    [TASK] Version 6.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3@blue-side.de',
     'state' => 'stable',
     'author_company' => '',
-    'version' => '6.2.4',
+    'version' => '6.3.0',
     'constraints' => [
         'depends' => [
         ]
