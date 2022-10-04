d0968f1897ec430d9f064496fcaeea57098b2f2a
SvenJuergens <SvenJuergens@users.noreply.github.com>
2022-10-04 23:30:55 +0200

    [TASK] Version 6.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3@blue-side.de',
     'state' => 'stable',
     'author_company' => '',
-    'version' => '6.5.2',
+    'version' => '6.6.0',
     'constraints' => [
         'depends' => [
         ],
