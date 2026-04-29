32aa9b93d91b6bb1d687bba70116782e4123b237
SvenJuergens <SvenJuergens@users.noreply.github.com>
2026-04-29 23:04:34 +0200

    [!!!][TASK] remove TYPO3 12 and add TYPO3 14 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /***************************************************************
  * Extension Manager/Repository config file for ext "sj_viewhelpers".
  *
@@ -9,7 +11,6 @@
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'SJ_Viewhelpers',
     'description' => 'some specific Viewhelpers',
